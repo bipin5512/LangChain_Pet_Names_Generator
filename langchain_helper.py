872e262309efa2a39fcc192f94e9c82160068998
Bipin Rai <bipin5512@gmail.com>
2023-10-15 15:13:15 +0530

    added logic to accept open api key as input

--- langchain_helper.py
+++ langchain_helper.py
@@ -7,8 +7,6 @@ from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 
 
-from dotenv import load_dotenv
-load_dotenv()
 
 def langchain_pet_name_generator():
     llm = OpenAI(temperature = 0.7, model = 'gpt-3.5-turbo')
@@ -17,11 +15,11 @@ def langchain_pet_name_generator():
 
     return name
 
-def langchain_prompt_chain_pet_name_generator(animal_type, pet_color):
-    llm = OpenAI(temperature = 0.7)
+def langchain_prompt_chain_pet_name_generator(openai_api_key, animal_type, pet_color):
+    llm = OpenAI(openai_api_key=openai_api_key, temperature = 0.7,)
     prompt_template_name =  PromptTemplate( 
         input_variables = ["animal_type", "pet_color"], 
-        template = "I have a {animal_type} pet in {pet_color} color and i want a cool name for him, please suggest five cool names. It should be Indian name"
+        template = "I have a {animal_type} pet in {pet_color} color and i want a cool name for him, please suggest five cool names. It should be an Indian names"
     )
 
     name_chain = LLMChain(llm=llm, prompt = prompt_template_name, output_key = "pet_name")
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 st.title ("Pets Name Generator")
 
+openai_api_key =  st.sidebar.text_input(label="What is your API Key?")
+
 animal_type = st.sidebar.selectbox("What is your pet?", ("Cat","Dog","Cow"))
 
 if animal_type == 'Cat':
@@ -14,8 +16,8 @@ if animal_type == 'Dog':
 if animal_type == 'Cow':
     pet_color = st.sidebar.text_area(label="What color is your Cow?", max_chars = 10)
 
-if pet_color:
-    response = lch.langchain_prompt_chain_pet_name_generator(animal_type=animal_type, pet_color=pet_color)
+if pet_color and openai_api_key:
+    response = lch.langchain_prompt_chain_pet_name_generator(openai_api_key=openai_api_key, animal_type=animal_type, pet_color=pet_color)
     st.text(response['pet_name'])
 
 
